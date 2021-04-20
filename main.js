b25d12a995539669dda96982b3d0d64d62f9bc91
alexpintodiaz <alexanderp143@hotmail.com>
2021-04-19 21:40:13 -0500

    26_03_2021_secuencia_funciones

---
+++ main.js
@@ -0,0 +1,25 @@
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+Saludar("Paula");
+Saludar("Carlos");
+Saludar("Daniela");
+Saludar("Astrid");
+Saludar("Alex");
+
+
+
+let saludar;
+saludar = "Paula";
+console.warn(`Hola ${saludar}`);
+saludar = "Carlos";
+console.warn(`Hola ${saludar}`);
+saludar = "Daniela";
+console.warn(`Hola ${saludar}`);
+saludar = "Astrid";
+console.warn(`Hola ${saludar}`);
+saludar = "Alex";
+console.warn(`Hola ${saludar}`);
+
+
